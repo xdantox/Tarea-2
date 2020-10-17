49379494a9d857ca766548701c58cb9813689dc7
xdantox <dantenav13@gmail.com>
2020-10-17 19:08:08 -0300

    Dejarlo bonito y arrego de errores

--- codep1.py
+++ codep1.py
@@ -15,26 +15,27 @@ gamma=2.704 # en 1/s, coeficiente de fricción
 # Implementando solucion usando RK4
 
 def func_pendulo(t, y):
-    output = np.array([y[1], -g / R * np.sin(y[0])] - gamma * y[1])
+    'Edo a integrar'
+    output = np.array([y[1], -g / R * np.sin(y[0]) - gamma * y[1]])
     return output
 
 
 def K1(func, dt, t_n, y_n):
-    """Constante K1 del algoritmo de Runge Kutta 2
+    """Constante K1 del algoritmo de Runge Kutta 4
     """
     output = dt * func(t_n, y_n)
     return output
 
 
 def K2(func, dt, t_n, y_n):
-    """Constante K2 del algoritmo de Runge Kutta 2
+    """Constante K2 del algoritmo de Runge Kutta 4
     """
     k1_n = K1(func, dt, t_n, y_n)
     k2_n = dt * func(t_n + dt/2, y_n + k1_n / 2)
     return k2_n
 
 def K3(func, dt, t_n, y_n):
-    """Constante K3 del algoritmo de Runge Kutta 2
+    """Constante K3 del algoritmo de Runge Kutta 4
     """
     k1_n = K1(func, dt, t_n, y_n)
     k2_n = dt * func(t_n + dt/2, y_n + k1_n / 2)
@@ -43,7 +44,7 @@ def K3(func, dt, t_n, y_n):
 
 
 def K4(func, dt, t_n, y_n):
-    """Constante K4 del algoritmo de Runge Kutta 2
+    """Constante K4 del algoritmo de Runge Kutta 4
     """
     k1_n = K1(func, dt, t_n, y_n)
     k2_n = dt * func(t_n + dt/2, y_n + k1_n / 2)
@@ -54,34 +55,58 @@ def K4(func, dt, t_n, y_n):
 
 
 def paso_rk4(func, dt, t_n, y_n):
-    output = y_n + 1/6 * (K1(func, dt, t_n, y_n) + 2 * K2(func, dt, t_n, y_n) +
-                2 * K3(func, dt, t_n, y_n) + K4(func, dt, t_n, y_n))
+    'Algoritmo de 1 paso RK4'
+    k1=K1(func, dt, t_n, y_n)
+    k2=K2(func, dt, t_n, y_n)
+    k3=K3(func, dt, t_n, y_n)
+    k4=K4(func, dt, t_n, y_n)
+    output = y_n + (1/6 * (k1 + 2 * k2 + 2 * k3 + k4))
     return output
 
+# Resolución
+'Paso temporal'
 
 dt = 0.01
+
+'Periodo de integracion'
+
 T = 2 * np.pi 
+
+'Intervalo de integración'
+
 t_eval_rk4 = np.arange(0, 2 * T, dt)
+
+'Definición de la forma de la solución'
+
 y_rk4 = np.zeros((len(t_eval_rk4), 2))
 
-# cond inicial
+# Condición inicial
+
 phi_0 = np.pi/20
 omega_0 = 0
 y_rk4[0] = [phi_0, omega_0]
+
+'Loop para dar los pasos de RK4 en el intervalo de integración'
 for i in range(1, len(t_eval_rk4)):
     y_rk4[i] = paso_rk4(func_pendulo, dt, t_eval_rk4[i-1], y_rk4[i-1])
 
+# Solución Analítica
+
 def peqosci(t):
-    det = np.sqrt(gamma ** 2 - 4 * (g / R))
+    det = np.sqrt((gamma) ** 2 - 4 * (g / R))
     lambda1 = (-gamma - det) / 2
     lambda2 = (-gamma + det) / 2
-    A1 = (phi_0 * lambda1) / (lambda1 - lambda2)
-    A2 = (phi_0 * lambda1) / (lambda2 - lambda1)
+    A1 = phi_0 / (1 - (lambda1 / lambda2))
+    A2 = -A1 * lambda1 / lambda2
     ypo = A1 * np.exp(lambda1 * t) + A2 * np.exp(lambda2 * t)
     return ypo
 
+# Gráfico:
+
 plt.clf()
-plt.plot(t_eval_rk4,y_rk4[:, 1], label='rk4')
+plt.plot(t_eval_rk4,y_rk4[:, 0], label='rk4')
 plt.plot(t_eval_rk4,peqosci(t_eval_rk4), label='peq osc')
+plt.xlabel('x', fontsize=15)
+plt.ylabel('y', fontsize=15)
 plt.legend()
 plt.show()
